9ca6717f8cb354f87e4f6dc4d65cfb8cd75171c0
FreezingTexture22 <andreisesiunin@gmail.com>
2023-03-28 17:52:59 +0200

    Stage2 completed

--- Stage2.java
+++ Stage2.java
@@ -3,35 +3,62 @@ package ReadabilityScore;
 import java.util.Scanner;
 
 public class Stage2 {
-	static int numberOfSentences = 0;
-	static int averageSentenceLength = 0;
+	static double numberOfSentences = 0;
+	static double numberOfWords = 0;
+	static double averageSentenceLength = 0;
+	static double averageNumberOfWords = 0;
 	static String[] array;
-	
+
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		
-		//get input and make an array, split by spaces
+
+		// get input and make an array, split by spaces
 		array = in.nextLine().split(" ");
-		
-		System.out.println(countNumberOfSentences());
-		
+
+//		System.out.println("Number of sentences: " + countNumberOfSentences());
+//		System.out.println("Number of words: " + countNumberOfWords());
+//		System.out.println("Average words in sentence: " + getAverageNumberOfWords());
+
+		countNumberOfSentences();
+		countNumberOfWords();
+		getAverageNumberOfWords();
+
+		if (averageNumberOfWords <= 10) {
+			System.out.println("EASY");
+		} else {
+			System.out.println("HARD");
+		}
+
 	}
-	
-	//count number of sentences ('!', '.', '?' - or last word without them)  
-	static int countNumberOfSentences() {
-		for(String i : array) {
-			if(i.matches("\\D*[!]") || i.matches("\\D*[.]") || i.matches("\\D*[?]") ) {
+
+	// count number of sentences ('!', '.', '?' - or last word without them)
+	static double countNumberOfSentences() {
+		for (String i : array) {
+			if (i.matches(".*[!]") || i.matches(".*[.]") || i.matches(".*[?]")) {
 				numberOfSentences++;
-				
+
 			}
 		}
-		
-		if (array[array.length-1].matches("^\\D*[^!]") && array[array.length-1].matches("^\\D*[^.]") && array[array.length-1].matches("^\\D*[^?]")) {
+
+		if (array[array.length - 1].matches("^.*[^!]") && array[array.length - 1].matches("^.*[^.]")
+				&& array[array.length - 1].matches("^.*[^?]")) {
 			numberOfSentences++;
-			
+
 		}
-		
+
 		return numberOfSentences;
 	}
-	
+
+	// count number of words
+	static double countNumberOfWords() {
+		numberOfWords = array.length;
+		return numberOfWords;
+	}
+
+	// get average number of words per sentence
+	static double getAverageNumberOfWords() {
+		averageNumberOfWords = numberOfWords / numberOfSentences;
+		return averageNumberOfWords;
+	}
+
 }
